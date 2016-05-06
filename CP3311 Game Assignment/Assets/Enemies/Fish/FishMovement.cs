44822b4dbea63dbeb6c8e8fe292cef588b4866b1
D-Hin <dominic.hin@my.jcu.edu.au>
2016-05-06 12:08:49 +1000

    File Update #6
    Imported BenHoogland’s EnemyFish package (i.e. altered crucscarp
    prefab), and made further adjustments to the MainScene.

---
+++ CP3311 Game Assignment/Assets/Enemies/Fish/FishMovement.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//vastly modified from initial example code from http://docs.unity3d.com/ScriptReference/Vector3.MoveTowards.html
+
+public class FishMovement : MonoBehaviour {
+
+	public Transform start;
+	public Transform jump1;
+	public Transform jump2;
+	public Transform end;
+	public float speed;
+	public float jumpHeight = 3f;
+	public float jumpHeightDropOffRate = 0.04f;
+	public float jumpSpeedBoost = 2f;
+
+	Vector3 target;
+	float jumpSpeed;
+	bool reverse;
+
+	void Start(){
+		target = jump1.position;
+		jumpSpeed = 0f;
+		reverse = false;
+	}
+
+	void FixedUpdate() {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target, step);
+		transform.Translate (Vector3.up * jumpSpeed * Time.deltaTime);
+		if (jumpSpeed > 0) {
+			jumpSpeed -= jumpHeightDropOffRate;
+		}
+		//right to left pass
+		if (!reverse) {
+			if (target == jump2.position) {
+				//corrects for position error jumpSpeed causes
+				if (Vector3.Distance (transform.position, target) < 0.1) {
+					transform.position = jump2.position;
+				}
+			}
+			//speed up for jump
+			if (transform.position == jump1.position) {
+				target = jump2.position;
+				speed += jumpSpeedBoost;
+				jumpSpeed = jumpHeight;
+			}
+			//slow down after jump
+			if (transform.position == jump2.position) {
+				target = end.position;
+				speed -= jumpSpeedBoost;
+				jumpSpeed = 0f;
+			}
+			if (transform.position == end.position) {
+				reverse = true;
+				//rotate fish and swim back
+				transform.Rotate(new Vector3(0, 180, 0));
+				target = jump2.position;
+			}
+		}
+		//left to right pass
+		if (reverse) {
+			if (target == jump1.position) {
+				//corrects for position error jumpSpeed causes
+				if (Vector3.Distance (transform.position, target) < 0.1) {
+					transform.position = jump1.position;
+				}
+			}
+			//speed up for jump
+			if (transform.position == jump2.position) {
+				target = jump1.position;
+				speed += jumpSpeedBoost;
+				jumpSpeed = jumpHeight;
+			}
+			//slow down after jump
+			if (transform.position == jump1.position) {
+				target = start.position;
+				speed -= jumpSpeedBoost;
+				jumpSpeed = 0f;
+			}
+			if (transform.position == start.position) {
+				reverse = false;
+				//rotate fish and swim back
+				transform.Rotate(new Vector3(0, -180, 0));
+				target = jump1.position;
+			}
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		//hurt the player if he touches the fishy
+		PlayerHealth health = other.gameObject.GetComponent<PlayerHealth>();
+		if (health != null)		{
+			health.TakeDamage (10);
+		}
+	}
+
+}
--- CP3311 Game Assignment/Assets/EthanMain/PlayerHealth.cs
+++ CP3311 Game Assignment/Assets/EthanMain/PlayerHealth.cs
@@ -3,115 +3,92 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-
-
 public class PlayerHealth : MonoBehaviour
 {
-    public int startingHealth = 100;                            // The amount of health the player starts the game with.
-    public int currentHealth;                                   // The current health the player has.
-    //public Slider healthSlider;                                 // Reference to the UI's health bar.
-    //public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
-    //public AudioClip deathClip;                                 // The audio clip to play when the player dies.
-    public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
+	public int startingHealth = 3;
+	public int currentHealth;
+	//public Slider healthSlider;
+	//public Image damageImage;
+	//public AudioClip deathClip;
+	public float flashSpeed = 5f;
+	// The speed the damageImage will fade at.
+	public Color flashColour = new Color (1f, 0f, 0f, 0.1f);
 
-
-    Animator anim;                                              // Reference to the Animator component.
-    //AudioSource playerAudio;                                    // Reference to the AudioSource component.
-    //PlayerMovement playerMovement;                              // Reference to the player's movement.
-   //PlayerShooting playerShooting;                              // Reference to the PlayerShooting script.
-    bool isDead;                                                // Whether the player is dead.
-    bool damaged;                                               // True when the player gets damaged.
+	Animator anim;
+	//AudioSource playerAudio;
+	private bool isDead;
+	private bool damaged;
 	float attackDelay;
 
+//	public GameObject player = GameObject.FindWithTag ("Player");
+//	public GameObject SpawnPoint = GameObject.FindWithTag ("Respawn");
+//	private Transform SpawnPoint;
 
-    void Awake ()
-    {
-        // Setting up the references.
-        anim = GetComponent <Animator> ();
-        //playerAudio = GetComponent <AudioSource> ();
-        //playerMovement = GetComponent <PlayerMovement> ();
-        //playerShooting = GetComponentInChildren <PlayerShooting> ();
-
-        // Set the initial health of the player.
-        currentHealth = startingHealth;
-	
+	void Awake ()
+	{
+		anim = GetComponent <Animator> ();
+		//playerAudio = GetComponent <AudioSource> ();
+		currentHealth = startingHealth;	
 		attackDelay = 0f;
-    }
 
+//		SpawnPoint.position = GameObject.FindWithTag ("Respawn").transform;
+	}
 
-    void Update () {
+	void Update ()
+	{
 		attackDelay += Time.deltaTime;
 
-        // If the player has just been damaged...
+		if (damaged) {
+			//damageImage.color = flashColour;
+		} else {
+			//damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
 
-        if(damaged)
-        {
-            // ... set the colour of the damageImage to the flash colour.
-            //damageImage.color = flashColour;
-        }
-        // Otherwise...
-        else
-        {
-            // ... transition the colour back to clear.
-            //damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-        }
+		damaged = false;
+	}
 
-
-        // Reset the damaged flag.
-        damaged = false;
-    }
-
-
-    public void TakeDamage (int amount)
-    {
-        // Set the damaged flag so the screen will flash.
-		if(attackDelay > 0.5f){
-        damaged = true;
-
-        // Reduce the current health by the damage amount.
-        currentHealth -= amount;
+	public void TakeDamage (int amount)
+	{
+		if (attackDelay > 0.5f) {
+			damaged = true;
+			currentHealth -= amount;
 			attackDelay = 0;
 		}
-        // Set the health bar's value to the current health.
-        //healthSlider.value = currentHealth;
 
-        // Play the hurt sound effect.
-        //playerAudio.Play ();
+		//healthSlider.value = currentHealth;
+		//playerAudio.Play ();
 
-        // If the player has lost all it's health and the death flag hasn't been set yet...
-        if(currentHealth <= 0 && !isDead)
-        {
-            // ... it should die.
-            Death ();
-        }
-    }
+		if (currentHealth <= 0 && !isDead) {
+			Death ();
+		}
+	}
 
+	void Death ()
+	{
+		isDead = true;
+//		anim.SetTrigger ("Die");
+//		playerAudio.clip = deathClip;
+//		playerAudio.Play ();
 
-    void Death ()
-    {
-        // Set the death flag so this function won't be called again.
-        isDead = true;
+		Respawn ();
+	}
 
-        // Turn off any remaining shooting effects.
-        //playerShooting.DisableEffects ();
+	public void RestartLevel ()
+	{
+//		SceneManager.LoadScene (0);
+	}
 
-        // Tell the animator that the player is dead.
-        anim.SetTrigger ("Die");
+	void OnTriggerEnter (Collider boundaryTrigger)
+	{
+		if (boundaryTrigger.gameObject.tag == "Boundary") {
+			isDead = true;
+		}
+	}
 
-        // Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
-        //playerAudio.clip = deathClip;
-        //playerAudio.Play ();
-
-        // Turn off the movement and shooting scripts.
-        //playerMovement.enabled = false;
-        //playerShooting.enabled = false;
-    }
-
-
-    public void RestartLevel ()
-    {
-        // Reload the level that is currently loaded.
-        //SceneManager.LoadScene (0);
-    }
+	public void Respawn ()
+	{
+//		this.gameObject.transform.position = SpawnPoint.transform.position;
+//		player.transform.position = SpawnPoint.transform.position;
+		isDead = false;
+	}
 }
