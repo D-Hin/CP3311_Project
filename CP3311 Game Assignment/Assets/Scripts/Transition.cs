802c63f20f3c6e627fb0558e7a8245c98c404016
D-Hin <dominic.hin@my.jcu.edu.au>
2016-05-10 14:13:25 +1000

    File Update #8
    Added a Day/Night cycle controller script to the MainScene lighting.
    
    Also instantiated an alternate PlayerCharacter prefab with the ability
    to switch between 1st & 3rd person views.
    (FirstPersonCamera does not orient properly with character body
    rotation, needs further scripting…)

---
+++ CP3311 Game Assignment/Assets/Scripts/Transition.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Transition : MonoBehaviour {
+
+	public int destination;
+
+	void Start () {}
+
+	void Update () {}
+
+	void OnTriggerEnter(Collider other)	{
+		//check if collider is the player
+		PlayerHealth health = other.gameObject.GetComponentInParent<PlayerHealth>();
+		if (health != null) {
+			SceneManager.LoadScene (destination, LoadSceneMode.Single);			
+		}
+	}
+}
